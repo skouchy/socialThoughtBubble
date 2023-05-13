9e686fb677f01c8d47a333edc54b8c48d99bc77d
skouch <skouchy24@gmail.com>
2023-05-13 01:27:00 -0700

    feat(server):cleaned up server file a little bit

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ app.use(require('./routes'));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-thought-bubble', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-    /*If the environment variable MONGODB_URI exists, like on Heroku when we deploy later, it will use that. Otherwise, it will short-circuit to the local MongoDB server's database at mongodb://localhost:27017/pizza-hunt. The second argument in the example is a set of configuration options Mongoose asks for more information about.
-    https://mongoosejs.com/docs/connections.html#options*/
 });
 
-// Use this to log mongo queries being executed!
+// This logs mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+});
